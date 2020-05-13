8ef13f8ebdd276671069fd8dc8e167bec822582f
EliasFarhan <elias.farhan@gmail.com>
2020-05-13 19:56:37 +0200

    Correct bug on Linux with ImGui, and set directly the VAO and VBO.

--- main/01_hello_triangle.cpp
+++ main/01_hello_triangle.cpp
@@ -20,11 +20,9 @@ public:
 		     0.5f, -0.5f, 0.0f, //bottom right
 		     0.0f,  0.5f, 0.0f //top
         };
-        unsigned int VAO;
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
-    	
-        unsigned int VBO;
+
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
